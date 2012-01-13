e07231b60432527bcde1245820b9ea0e10347b88
Brian Hann <emailc0bra@gmail.com>
2012-01-13 17:58:50 -0600

    Adding script that parses moby dick's text & generates an image

---
+++ script/image.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use HTML::WordCloud;
+use File::Slurp;
+use Data::Dumper;
+
+my $moby = read_file('./script/moby_dick.txt');
+
+#my @words = qw/this is a bunch of words/;
+my @words = split /\s+/, $moby;
+
+#my %wordhash = map { shift @words => $_ } (1 .. ($#words+1));
+
+my $wc = new HTML::WordCloud(prune_boring => 0);
+
+$wc->words(\@words);
+
+#print Dumper($wc->words);
+
+binmode STDOUT;
+
+my $img = $wc->cloud();
+
+print $img->png;
