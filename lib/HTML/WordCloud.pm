ff355c9d66015da3acc345678e770b01ff4439b3
Brian Hann <emailc0bra@gmail.com>
2012-01-13 18:00:20 -0600

    Various updates and changes. It generates images now!

--- lib/HTML/WordCloud.pm
+++ lib/HTML/WordCloud.pm
@@ -6,12 +6,21 @@ use warnings;
 
 use Carp qw(carp croak confess);
 use Params::Validate qw(:all);
+use List::Util qw(sum);
 use Data::Types qw(:int :float);
 use Search::Dict;
+use GD;
+use GD::Text::Align;
 
 our $golden_ratio_conjugate = 0.618033988749895;
 
+our $font_path = "./share/fonts/";
 our $boring_word_dict_file = "./share/pos/part-of-speech.txt";
+
+our @fonts = qw(
+	GeosansLight.ttf
+	GeosansLight-Oblique.ttf
+);
 
 =head1 NAME
 
@@ -57,6 +66,7 @@ sub new {
     my $class = ref( $proto ) || $proto;
     my $self = { #Will need to allow for params passed to constructor
 			words        => {},
+			word_list    => [],
       word_count   => $opts{word_count},
       prune_boring => $opts{prune_boring},
     };
@@ -77,23 +87,43 @@ sub words {
 	my $self = shift;
 	
 	my @opts = validate_pos(@_,
-  	{ type => HASHREF, optional => 0 },
+  	{ type => HASHREF | ARRAYREF, optional => 1 }, # \%words
   );
   
-  my %words = %{ $opts[0] };
+  # Return words if no argument is specified
+  if (! $opts[0]) { return $self->{words}; }
+  
+  my %words = ();
+  if (ref($opts[0]) eq 'HASH') {
+  	%words = %{ $opts[0] };
+	}
+	elsif (ref($opts[0]) eq 'ARRAY') {
+		foreach my $word(map { lc } @{ $opts[0] }) {
+			$words{ $word }++;
+		}
+	}
   
   # Blank out the current word list;
   $self->{word} = {};
   
   # Sort the words by count and let N number of words through, based on $self->{word_count}
   my $word_count = 1;
-  foreach my $word (sort { $words{$b} <=> $words{$a} } keys %words) {
+  foreach my $word (map { lc } sort { $words{$b} <=> $words{$a} } keys %words) {
   	last if $word_count > $self->{word_count};
   	
   	my $count = $words{$word};
   	
+  	if ($word_count == 1) {
+  		$self->{max_count} = $count;
+  	}
+  	
   	# Add this word to our list of words
   	$self->{words}->{$word} = $count;
+  	
+  	push(@{ $self->{word_list} }, {
+  		word  => $word,
+  		count => $count
+  	});
   	
   	$word_count++;
   }
@@ -108,12 +138,74 @@ Make the word cloud! Returns an image file location
 sub cloud {
 	my $self = shift;
 	
-	self->_prune_boring_words();
+	# Remove boring words from our wordlist
+	$self->_prune_boring_words() if $self->{prune_boring};
 	
-	return "";
+	# Create the image object
+	my $gd = GD::Image->new(800, 600, 1); # 1 = truecolor
+	
+	my $white = $gd->colorAllocate(255, 255, 255);
+	my $gray  = $gd->colorAllocate(100, 100, 100);
+	my $black = $gd->colorAllocate(0, 0, 0);
+	
+	my $rand_colors = $self->_random_palette(count => 5);
+	my @palette = ();
+	foreach my $c (@$rand_colors) {
+		my $newc = $gd->colorAllocate($c->[0], $c->[1], $c->[2]);
+		push @palette, $newc;
+	}
+	
+	# make the background transparent and interlaced  
+	$gd->transparent($gray);
+  $gd->interlaced('true');
+	
+	# Array of GD::Text::Align objects that we will move around and then draw
+	my @texts = ();
+	
+	# Max font size in points (10% of image height)
+	my $max_points = ($gd->height * 72 / 96) * .20;
+	
+	# Scaling modifier for font sizes
+	my $scaling = $max_points / $self->{max_count};
+	
+	# For each word we have
+	my @areas = ();
+	my $loop = 0;
+	while (my ($word, $count) = each %{ $self->{words} }) {
+		my $text = new GD::Text::Align($gd);
+		
+		# Use a random color
+		my $color = $palette[ rand @palette ];
+		$text->set(color => $color);
+		
+		# Use a random font
+		my $font = $font_path . $fonts[ rand @fonts ];
+			unless (-f $font) { carp "Found file '$font' not found"; }
+		
+		my $size = $count * $scaling;
+		
+		$text->set_font($font, $size);
+		
+		# Set the text to this word
+		$text->set_text($word);
+		
+		push(@texts, $text);
+		
+		my ($w, $h) = $text->get('width', 'height');
+		
+		push(@areas, $w * $h);
+		
+		my @bounding = $text->draw(rand $gd->width * .8, rand $gd->height * .8, 0);
+		$loop++;
+	}
+	
+	my $total_area = sum @areas;
+	
+	# Return the image as PNG content
+	return $gd;
 }
 
-=head2 random_palette($color_count, [$saturation, $value])
+=head2 _random_palette($color_count, [$saturation, $value])
 
 Generate C<$color_count> number of RGB colors. C<$color_count> must be an integer. C<$saturation> and C<$value> are optional floating point values from 0.0 to 1.0.
 They default to 0.5 and 0.95 respectively.
@@ -123,7 +215,7 @@ Return value: C<\@colors>
 =cut
 
 # Stolen from: http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
-sub random_palette {
+sub _random_palette {
 	my $self = shift;
 	
 	my %opts = validate(@_, {
@@ -185,6 +277,11 @@ sub _prune_boring_words {
 	my $self = shift;
 	
 	my @opts = validate_pos(@_, { type => HASHREF, optional => 1 });
+	
+	if (! -f $boring_word_dict_file) {
+		carp "Boring word file '$boring_word_dict_file' not found, not pruning any words";
+		return;
+	}
 	
 	# Either use the words supplied to the subroutine or use what we have in the object
 	my $words = {};
