179a2c49999bf4bbb65ee4c745cb82c4e5d04c0c
Brian Hann <emailc0bra@gmail.com>
2012-01-27 16:01:52 -0600

    Renamed bug tracker location to image-wordcloud

--- lib/Image/WordCloud.pm
+++ lib/Image/WordCloud.pm
@@ -723,7 +723,7 @@ Brian Hann, C<< <brian.hann at gmail.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-html-wordcloud at rt.cpan.org>, or through
+Please report any bugs or feature requests to C<bug-image-wordcloud at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Image-WordCloud>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
